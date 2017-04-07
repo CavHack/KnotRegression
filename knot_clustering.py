42ea1b19a2df881a58128562f6ecb3495fd1694f
CAVHACK (Karl W) <karl@blackesquirel.com>
2017-04-06 21:20:26 -0700

    KMeans
     Apply KMeans for clustering a dataset given as input, and the number
    of clusters (K).
        Input: x1, ..., xn where x in R^d, and K
        Output: Vector c of cluster assignments, and K mean vectors mu

---
+++ knot_clustering.py
@@ -0,0 +1,19 @@
+import sys
+import numpy as np
+
+def load_data(input_file):
+    """"
+    Load the dataset of interest.
+    """"
+
+    data = np.genfromtxt(input_file, delimiter=',', skip_header=0, names=None)
+    return data
+
+
+
+    def KMeans(X, K=5, maxit=10, saveLog = True):
+        """"
+     Apply KMeans for clustering a dataset given as input, and the number of clusters (K).
+    Input: x1, ..., xn where x in R^d, and K
+    Output: Vector c of cluster assignments, and K mean vectors mu
+        """"
