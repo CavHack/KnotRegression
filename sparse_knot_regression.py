b9bcf060c9ed87c68ed2a8a466738f72ed3c1e06
CAVHACK (Karl W) <karl@blackesquirel.com>
2017-04-06 20:29:51 -0700

    Range:10 items
    Compute the associative transpose of sigma inversion

---
+++ sparse_knot_regression.py
@@ -0,0 +1,90 @@
+import numpy as np
+import sys
+
+np.set_printoptions(precision=16)
+
+#ridge regression
+def RR(X, y, lam=1, centering=False, standarize=False):
+    """
+    Minimize in closed form the error function embedded in a sum-of-squares error
+    term. These are shrinkage methods or weight decay methods. Apply RR and return
+    this weight vector.
+    """
+    n_features = X.shape[1]
+
+    #preprocessing
+    if centering:
+        y= y - np.mean(y)
+        xmean = np.mean(X, axis=0)
+
+    if standarize:
+        sigma = np.var(X, axis=0)
+        X = (X - xmean)//sigma
+
+        I = np.identity(n_features)
+        #I[-1, 1] = 0
+
+        A = np.linalg.solve(lam*I + np.dot(np.transpose(X[:, :]), X[:, :]), np.transpose(X[:, :]))
+
+        //return the weights vector.
+        wRR = np.dot(A, y)
+        return wRR
+
+ #active learning
+ def AL(X_train, X_test, lam=1, sigma2=1):
+     """
+     Implements the Active Learning Scheme as seen in class
+     Returns a list of the first 10 chosen vectors in X_test
+     """
+     X = np.copy(X_train)
+     D= np.copy(X_test)
+     selected = []
+     n_features = X.shape[1]
+     I = np.identity(n_features)
+     #I[-1, 1] = 0
+
+        for i in xrange(10):
+
+            sigma02_max = float('-Inf')
+            idx_max = None
+            for idx in range (len(D)):
+                if idx not in selected:
+                    #identity.
+                    x0 = D[idx]
+                    cov = np.linalg.inv(lam*I + 1.0/sigma2 * (np.dot(x0, np.transpose(x0)) + np.dot(np.transpose(x), X)))
+                    sigma2 = sigma2 + np.dot(np.transpose(x0), np.dot(cov, x0))
+
+
+            #Steepest sigma descent. Find Maxima.
+            if sigma02 > sigma02_max:
+                sigma02_max = sigma02
+                idx_max = idx
+
+        selected.append(idx_max)
+        X = np.concatenate((X, [D[idx_max]]))
+
+        selected = [idx + 1 for idx in selected]
+
+        return selected
+
+ if __name__ == '__main__':
+     if len(sys.argv) != 6
+     print 'Usage: python hw1_regression.py <lambda> <sigma2> <X_train.csv> <y_train.csv> <X_test.csv>'
+     sys.exit(0)
+
+     lam = float(sys.arg[1])
+     sigma2 = float(sys.argv[2])
+     X_train = np.genfromtxt(sys.argv[3], delimiter=',', skip_header=0, names=None)
+     X_train = np.genfromtxt(sys.argv[4], delimiter=',', skip_header=0, names=None)
+     X_test = np.genfromtxt(sys.argv[5], delimiter=',', skip_header=0, names=None)
+     wRR = RR(X_train, y_train, lam)
+     active = AL(X_train, X_test, lam, sigma2)
+
+     with open("wRR_ " + sys.argv[1] + ".csv", "w") as text_file:
+         for w in wRR:
+             text_file.write(str(w) + "\n")
+
+             with open("active_" + sys.argv[1] + "_" + sys.argv[2] + ".csv", "w")  as text_file:
+                 text_file.write(("%d") % active[0])
+                 for idx in range(1, 10):
+                     text_file.write((",%d") % active[idx])
